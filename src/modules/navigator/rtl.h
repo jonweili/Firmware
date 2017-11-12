a9ed78e95216fa02973f0d25401363c123270406
Daniel Agar <daniel@agar.ca>
2017-11-12 14:52:05 -0500

    Navigator RTL fully initialize mission item in each state

--- src/modules/navigator/rtl.h
+++ src/modules/navigator/rtl.h
@@ -41,13 +41,6 @@
 #ifndef NAVIGATOR_RTL_H
 #define NAVIGATOR_RTL_H
 
-#include <controllib/blocks.hpp>
-#include <controllib/block/BlockParam.hpp>
-
-#include <navigator/navigation.h>
-#include <uORB/topics/home_position.h>
-#include <uORB/topics/vehicle_global_position.h>
-
 #include "navigator_mode.h"
 #include "mission_block.h"
 
